0cda84314558750267dc668c3b3f04f378edfc59
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:41:41 -0300

    CONTROLLER - busca vendas por id

--- backend/src/controllers/sales.controller.js
+++ backend/src/controllers/sales.controller.js
@@ -6,6 +6,19 @@ const getAll = async (_req, res) => {
   return res.status(httpMap[allSales.status]).json(allSales.data);
 };
 
+const getSaleById = async (req, res) => {
+  const { id } = req.params;
+  const products = await salesService.getSaleById(id);
+
+  if (products.status !== 'SUCCESS') {
+    return res
+      .status(httpMap[products.status]).json({ message: products.data }); 
+  }
+  
+  return res.status(httpMap[products.status]).json(products.data);
+};
+
 module.exports = {
   getAll,
+  getSaleById,
 };
