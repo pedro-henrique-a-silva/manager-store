44c0e9c32a98d3c28b889ebe746eb86d0937aef7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:41:29 -0300

    SERVICE - busca vendas por id

--- backend/src/services/sales.service.js
+++ backend/src/services/sales.service.js
@@ -5,6 +5,15 @@ const getAll = async () => {
   return { status: 'SUCCESS', data: allSales };
 };
 
+const getSaleById = async (saleId) => {
+  const sale = await salesModel.getSaleById(saleId);
+
+  if (sale.length === 0) return { status: 'NOT_FOUND', data: 'Sale not found' };
+
+  return { status: 'SUCCESS', data: sale };
+};
+
 module.exports = {
   getAll,
+  getSaleById,
 };
