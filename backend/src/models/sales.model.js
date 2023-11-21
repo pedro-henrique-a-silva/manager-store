d39d32ce80b94b200dc554cc7c65dc9008820b59
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:41:12 -0300

    Model - Função busca vendas por id

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -11,6 +11,18 @@ const getAll = async () => {
   return camelize(allSales);
 };
 
+const getSaleById = async (saleId) => {
+  const [sale] = await connection.execute(`
+    SELECT product_id, date, quantity FROM sales_products sp
+    INNER JOIN products p ON p.id = sp.product_id
+    INNER join sales s ON s.id = sp.sale_id
+    WHERE sale_id = ?
+  `, [saleId]);
+
+  return camelize(sale);
+};
+
 module.exports = {
   getAll,
+  getSaleById,
 };
