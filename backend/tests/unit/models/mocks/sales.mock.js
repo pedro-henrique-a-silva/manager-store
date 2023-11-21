337b100b24437093e28901480a18fb350132c3fe
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:42:19 -0300

    MOCK - busca vendas por id

--- backend/tests/unit/models/mocks/sales.mock.js
+++ backend/tests/unit/models/mocks/sales.mock.js
@@ -1,4 +1,27 @@
+const DATA_VENDA = '2023-11-21T20:11:59.000Z';
+
 const allSalesFromDB = [
+  {
+    saleId: 1,
+    productId: 1,
+    date: DATA_VENDA,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    productId: 2,
+    date: DATA_VENDA,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    productId: 3,
+    date: DATA_VENDA,
+    quantity: 15,
+  },
+];
+
+const salesByIdFromDB = [
   {
     saleId: 1,
     productId: 1,
@@ -11,14 +34,9 @@ const allSalesFromDB = [
     date: '2023-11-21T20:11:59.000Z',
     quantity: 10,
   },
-  {
-    saleId: 2,
-    productId: 3,
-    date: '2023-11-21T20:11:59.000Z',
-    quantity: 15,
-  },
 ];
 
 module.exports = {
   allSalesFromDB,
+  salesByIdFromDB,
 };
