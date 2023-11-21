161e1a4a41a277333192d1bbaab9775d5c3c605b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 16:36:17 -0300

    Criando camada service de produtos

--- backend/src/services/products.service.js
+++ backend/src/services/products.service.js
@@ -3,9 +3,19 @@ const { productsModel } = require('../models');
 const getAll = async () => {
   const allProducts = await productsModel.getAll();
   const allProductsOrdered = allProducts.sort((a, b) => a.id - b.id);
-  return allProductsOrdered;
+  
+  return { status: 'SUCCESS', data: allProductsOrdered };
+};
+
+const getProductById = async (productId) => {
+  const product = await productsModel.getProductById(productId);
+
+  if (!product) return { status: 'NOT_FOUND', data: 'Product not found' };
+
+  return { status: 'SUCCESS', data: product };
 };
 
 module.exports = {
   getAll,
+  getProductById,
 };
