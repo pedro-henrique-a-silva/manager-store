2fdcb19565c493523bdaac277f20a94e0f895299
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:21:45 -0300

    Mock retorno todas as vendas

---
+++ backend/tests/unit/models/mocks/sales.mock.js
@@ -0,0 +1,24 @@
+const allSalesFromDB = [
+  {
+    saleId: 1,
+    productId: 1,
+    date: '2023-11-21T20:11:59.000Z',
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    productId: 2,
+    date: '2023-11-21T20:11:59.000Z',
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    productId: 3,
+    date: '2023-11-21T20:11:59.000Z',
+    quantity: 15,
+  },
+];
+
+module.exports = {
+  allSalesFromDB,
+};
