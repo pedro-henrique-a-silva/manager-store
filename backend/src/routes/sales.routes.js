5726706bfbaf9d2a8f8d2a56a126a0ae5438dc54
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 17:41:53 -0300

    ROTA - busca vendas por id

--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -4,5 +4,6 @@ const { salesController } = require('../controllers');
 const salesRouter = express.Router();
 
 salesRouter.get('/', salesController.getAll);
+salesRouter.get('/:id', salesController.getSaleById);
 
 module.exports = salesRouter;
