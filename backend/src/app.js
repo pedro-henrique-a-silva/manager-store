b4b890a2dae7c4dd47e4f728b43225a225d33113
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-21 15:14:17 -0300

    adicionando rota produtos ao app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
@@ -6,5 +7,7 @@ const app = express();
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
+
+app.use('/products', routes.productsRouter);
 
 module.exports = app;
