233aea83c6e434d76b45fc7937274fd8f6d68ada
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 15:33:21 -0300

    Rotas - rota de registro novas vendas

--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -4,6 +4,7 @@ const { salesController } = require('../controllers');
 const salesRouter = express.Router();
 
 salesRouter.get('/', salesController.getAll);
+salesRouter.post('/', salesController.registerSales);
 salesRouter.get('/:id', salesController.getSaleById);
 
 module.exports = salesRouter;
