baff0b4ff5515064cd1e7580fb706500a9a2e82d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 17:13:48 -0300

    Tests Middleware - validateRegisterSales

---
+++ backend/tests/unit/middlewares/sales.middlewares.test.js
@@ -0,0 +1,70 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const { salesMiddlewares } = require('../../../src/middlewares');
+const salesMock = require('./mock/sales.mock');
+
+const { expect } = chai;
+
+chai.use(sinonChai);
+
+// const next = sinon.stub().returns(); // crie um stub
+// >
+// myMiddlewares.validateMiddleware(req, res, next); // passe o `next` para o middleware junto com o `req` e `res`
+// >
+// expect(next).to.have.been.calledWith(); 
+
+describe('Testando sales - Middleware', function () {
+  it('Testando se não e possivel cadastrar venda com sucesso', async function () {
+    const req = {
+      body: salesMock.salesToRegisterReqCorrect,
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const next = sinon.stub().returns();
+
+    await salesMiddlewares.validateProductToRegister(req, res, next);
+    expect(next).to.have.been.calledWith();
+  });
+  it('Testando se não e possivel cadastrar venda sem productId', async function () {
+    const req = {
+      body: salesMock.salesToRegisterReqWithoutProductId,
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const next = sinon.stub().returns();
+
+    await salesMiddlewares.validateProductToRegister(req, res, next);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({ message: '"productId" is required' });
+  });
+
+  it('Testando se não e possivel cadastrar venda sem quantity', async function () {
+    const req = {
+      body: salesMock.salesToRegisterReqWithoutQuantity,
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const next = sinon.stub().returns();
+
+    await salesMiddlewares.validateProductToRegister(req, res, next);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({ message: '"quantity" is required' });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
