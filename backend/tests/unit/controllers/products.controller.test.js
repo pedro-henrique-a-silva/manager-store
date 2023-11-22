cefb9c2958c10ca12d0b7786485f1374e978d2c0
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 19:18:03 -0300

    Testes controller - Testando delete de produto

--- backend/tests/unit/controllers/products.controller.test.js
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -7,6 +7,9 @@ const { expect } = chai;
 const {
   registerProductFromServiceSuccess,
   registerProductNameLengthSmallerThanFive,
+  productDeletedWithSuccess,
+  productDeletedError,
+  productByIdFromService,
 } = require('./mocks/products.mock');
 const { productsService } = require('../../../src/services');
 const { productsController } = require('../../../src/controllers');
@@ -30,7 +33,39 @@ describe('Testando products - CONTROLLER', function () {
     expect(res.json).to.have.been.calledWith(registerProductFromServiceSuccess.data);
   });
 
-  it('Testando registro de produtos com falhando com nome com menos de 5 caracteres', async function () {
+  it('Testando busca de produtos por Id', async function () {
+    sinon.stub(productsService, 'getProductById').resolves(productByIdFromService);
+    const req = {
+      params: { id: 1 },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.getProductById(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith();
+  });
+
+  it('Testando se busca de produtos por Id que não existe falha', async function () {
+    sinon.stub(productsService, 'getProductById').resolves({ status: 'NOT_FOUND', data: 'Product not found' });
+    const req = {
+      params: { id: 1999 },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.getProductById(req, res);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith();
+  });
+
+  it('Testando registro de produtos falhando  com com nome com menos de 5 caracteres', async function () {
     sinon.stub(productsService, 'registerProd').resolves(registerProductNameLengthSmallerThanFive);
     const req = {
       body: { name: 'Pro' },
@@ -48,6 +83,38 @@ describe('Testando products - CONTROLLER', function () {
     await productsController.registerProd(req, res);
     expect(res.status).to.have.been.calledWith(422);
     expect(res.json).to.have.been.calledWith(returnMessage);
+  });
+
+  it('Testando se é posssivel deletar um produto com sucesso', async function () {
+    sinon.stub(productsService, 'deletProductById').resolves(productDeletedWithSuccess);
+    const req = {
+      params: { id: '1' },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.deletProductById(req, res);
+    expect(res.status).to.have.been.calledWith(204);
+    expect(res.json).to.have.been.calledWith();
+  });
+
+  it('Testando se não é posssivel deletar um produto que não exista', async function () {
+    sinon.stub(productsService, 'deletProductById').resolves(productDeletedError);
+    const req = {
+      params: { id: '1' },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productsController.deletProductById(req, res);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
   });
 
   afterEach(function () {
