4d1c6773fcc810651aeaf7a1d1e2c81668914b5f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 14:17:12 -0300

    Tratando integridade do dado na camada service

--- backend/src/services/products.service.js
+++ backend/src/services/products.service.js
@@ -16,11 +16,17 @@ const getProductById = async (productId) => {
 };
 
 const registerProd = async (productName) => {
+  if (productName.length < 5) {
+    return { 
+      status: 'UNABLE_TO_PROCESS', 
+      data: '"name" length must be at least 5 characters long' };
+  }
+
   const product = await productsModel.registerProd(productName);
 
   if (!product) return { status: 'NOT_FOUND', data: 'Product not found' };
 
-  return { status: 'CREATED', data: product };
+  return { status: 'CREATED', data: { id: product, name: productName } };
 };
 
 module.exports = {
