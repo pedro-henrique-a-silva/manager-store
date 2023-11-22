6cabf233ed4df07f9d49bca5eb799f8c3f005e05
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 17:14:32 -0300

    Tests Service - testando service registrar vendas

--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -1,12 +1,18 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { salesModel } = require('../../../src/models');
+const { productsModel } = require('../../../src/models');
 const { salesService } = require('../../../src/services');
-const salesMocks = require('./mocks/products.mock');
+const salesMocks = require('./mocks/sales.mock');
 
 describe('Testando sales - SERVICE', function () {
   it('Testando registro de vendas bem sucedido', async function () {
     sinon.stub(salesModel, 'registerSales').resolves(5);
+    sinon.stub(productsModel, 'getProductById')
+      .onFirstCall()  
+      .resolves({ id: 1, name: 'produto1' })
+      .onSecondCall()
+      .resolves({ id: 2, name: 'produto2' });
 
     const registerSaleData = await salesService.registerSales(salesMocks.salesToRegister);
 
@@ -14,5 +20,25 @@ describe('Testando sales - SERVICE', function () {
     expect(registerSaleData).to.have.property('data');
     expect(registerSaleData.data).to.have.property('id');
     expect(registerSaleData.data).to.have.property('itemsSold');
+  });
+
+  it('Testando se não registra vendas caso produto não exista', async function () {
+    sinon.stub(salesModel, 'registerSales').resolves(5);
+    sinon.stub(productsModel, 'getProductById')
+      .onFirstCall()  
+      .resolves({ id: 1, name: 'produto1' })
+      .onSecondCall()
+      .resolves(undefined);
+
+    const registerSaleData = await salesService.registerSales(salesMocks.salesToRegister);
+
+    expect(registerSaleData).to.have.property('status');
+    expect(registerSaleData).to.have.property('data');
+    expect(registerSaleData.status).to.be.equal('NOT_FOUND');
+    expect(registerSaleData.data).to.be.deep.equal({ message: 'Product not found' });
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
