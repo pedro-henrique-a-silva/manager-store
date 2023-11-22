425e2985f647875cdc9cb688acd81b8131993d33
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-22 14:16:12 -0300

    Middleware - validate product

---
+++ backend/src/middlewares/products.middleware.js
@@ -0,0 +1,13 @@
+const validateProductName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateProductName,
+};
