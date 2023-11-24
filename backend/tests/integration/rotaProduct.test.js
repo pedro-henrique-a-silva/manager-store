f9d5e6198e2d8ca4d4a32e33da80a745623bedf1
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-24 18:57:03 -0300

    Testes de integração rota produtos

---
+++ backend/tests/integration/rotaProduct.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+const connection = require('../../src/models/connection');
+
+const productsMock = require('./mocks/products.mock');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste Integração Rotas de produtos', function () {
+  it('Testando rota /products de listagem de produtos', async function () {
+    sinon.stub(connection, 'execute').resolves([productsMock.allProductsFromDB]);
+    const response = await chai
+      .request(app)
+      .get('/products');
+
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.an('array');
+  });
+});
