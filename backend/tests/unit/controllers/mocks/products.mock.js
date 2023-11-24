ade618dac5d87a0d0ccf296343b480bc9c0b117a
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-24 18:58:36 -0300

    add Mock allProductsFromService

--- backend/tests/unit/controllers/mocks/products.mock.js
+++ backend/tests/unit/controllers/mocks/products.mock.js
@@ -13,10 +13,26 @@ const productDeletedWithSuccess = { status: 'SUCCESS_NO_CONTENT', data: '' };
 const productDeletedError = { status: 'NOT_FOUND', data: { message: 'Product not found' } };
 
 const productByIdFromService = { status: 'SUCCESS', data: { id: 1, name: 'Martelo de Thor' } };
+
+const allProductsFromService = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+  {
+    id: 3,
+    name: 'Escudo do CapitÃ£o AmÃ©rica',
+  },
+];
 module.exports = {
   registerProductFromServiceSuccess,
   registerProductNameLengthSmallerThanFive,
   productDeletedWithSuccess,
   productDeletedError,
   productByIdFromService,
+  allProductsFromService,
 };
