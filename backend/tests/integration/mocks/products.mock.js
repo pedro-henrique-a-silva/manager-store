fab9b77a729a554a41aa69d14ab0b71e913c4de4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-24 18:57:20 -0300

    Mocks testes integração produtos

---
+++ backend/tests/integration/mocks/products.mock.js
@@ -0,0 +1,29 @@
+const allProductsFromDB = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+  {
+    id: 3,
+    name: 'Escudo do CapitÃ£o AmÃ©rica',
+  },
+];
+
+const productFromDB = {
+  id: 1,
+  name: 'Martelo de Thor',
+};
+
+const createdProduct = { insertId: 5 };
+const updatedProduct = { affectedRows: 1 };
+
+module.exports = {
+  allProductsFromDB,
+  productFromDB,
+  createdProduct,
+  updatedProduct,
+};
