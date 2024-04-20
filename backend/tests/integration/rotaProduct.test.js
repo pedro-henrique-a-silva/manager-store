43f714187f44d1b7251b0c7a933bd7c6606229b3
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-04-20 16:27:59 -0300

    teste rota product

--- backend/tests/integration/rotaProduct.test.js
+++ backend/tests/integration/rotaProduct.test.js
@@ -1,27 +1,27 @@
-// const chai = require('chai');
-// const sinon = require('sinon');
-// const chaiHttp = require('chai-http');
-// const app = require('../../src/app');
-// const connection = require('../../src/models/connection');
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+const connection = require('../../src/models/connection');
 
-// const productsMock = require('./mocks/products.mock');
+const productsMock = require('./mocks/products.mock');
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe('Teste Integração Rotas de produtos', function () {
-//   // it('Testando rota /products de listagem de produtos', async function () {
-//   //   sinon.stub(connection, 'execute').resolves([productsMock.allProductsFromDB]);
-//   //   const response = await chai
-//   //     .request(app)
-//   //     .get('/products');
+describe('Teste Integração Rotas de produtos', function () {
+  it('Testando rota /products de listagem de produtos', async function () {
+    sinon.stub(connection, 'execute').resolves([productsMock.allProductsFromDB]);
+    const response = await chai
+      .request(app)
+      .get('/products');
 
-//   //   expect(response).to.have.status(200);
-//   //   expect(response.body).to.be.an('array');
-//   // });
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.an('array');
+  });
 
-//   // afterEach(function () {
-//   //   sinon.restore();
-//   // });
-// });
+  afterEach(function () {
+    sinon.restore();
+  });
+});
